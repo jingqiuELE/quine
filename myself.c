91de14c307b2deef461bce85e29c7eb084808779
Jing Qiu <aqiu0720@gmail.com>
2017-12-18 12:47:40 +0800

    reduce program length.

--- myself.c
+++ myself.c
@@ -1,62 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-void myformat(const char *s, char *fmt_s) {
-    char buff[] = "    prs(\"";
-    int len = strlen(s);
-    int i, j;
-    strcpy(fmt_s, buff);
-    j = strlen(fmt_s);
-    for (i=0; i<len; i++) {
-        if (s[i] == '\"' || s[i] == '\\') {
-            fmt_s[j++] = '\\';
+void puts_m(const char *s) {
+    int i;
+    for(i=0; i<strlen(s); i++) {
+        if (s[i] == 10) {
+            putchar(92);
+            putchar(110);
+        } else {
+            putchar(s[i]);
         }
-        fmt_s[j++] = s[i];
     }
-    strcat(fmt_s, "\", (char *)&store);\n");
 }
-void prs(const char *s, char *store) {
-    char format_s[256] = {0};
-    if(0 == strcmp(s, "%s")) {
-        printf("%s", store);
-        printf("    prs(\"%%s\", (char *)&store);\n");
-        printf("}\n");
-    } else {
-        printf("%s\n", s);
-        myformat(s, (char *)&format_s);
-        strcat(store, format_s);
-    }
+void prs(char *s) {
+    const char tail[] = {34, 59, 10, 32, 32, 32, 32, 112, 114, 115, 40, 115, 41, 59, 10, 125};
+    fputs(s, stdout);
+    putchar(34);
+    puts_m(s);
+    puts(tail);
 }
 void main() {
-    char store[4096] = {0};
-    prs("#include <stdio.h>", (char *)&store);
-    prs("#include <string.h>", (char *)&store);
-    prs("void myformat(const char *s, char *fmt_s) {", (char *)&store);
-    prs("    char buff[] = \"    prs(\\\"\";", (char *)&store);
-    prs("    int len = strlen(s);", (char *)&store);
-    prs("    int i, j;", (char *)&store);
-    prs("    strcpy(fmt_s, buff);", (char *)&store);
-    prs("    j = strlen(fmt_s);", (char *)&store);
-    prs("    for (i=0; i<len; i++) {", (char *)&store);
-    prs("        if (s[i] == '\\\"' || s[i] == '\\\\') {", (char *)&store);
-    prs("            fmt_s[j++] = '\\\\';", (char *)&store);
-    prs("        }", (char *)&store);
-    prs("        fmt_s[j++] = s[i];", (char *)&store);
-    prs("    }", (char *)&store);
-    prs("    strcat(fmt_s, \"\\\", (char *)&store);\\n\");", (char *)&store);
-    prs("}", (char *)&store);
-    prs("void prs(const char *s, char *store) {", (char *)&store);
-    prs("    char format_s[256] = {0};", (char *)&store);
-    prs("    if(0 == strcmp(s, \"%s\")) {", (char *)&store);
-    prs("        printf(\"%s\", store);", (char *)&store);
-    prs("        printf(\"    prs(\\\"%%s\\\", (char *)&store);\\n\");", (char *)&store);
-    prs("        printf(\"}\\n\");", (char *)&store);
-    prs("    } else {", (char *)&store);
-    prs("        printf(\"%s\\n\", s);", (char *)&store);
-    prs("        myformat(s, (char *)&format_s);", (char *)&store);
-    prs("        strcat(store, format_s);", (char *)&store);
-    prs("    }", (char *)&store);
-    prs("}", (char *)&store);
-    prs("void main() {", (char *)&store);
-    prs("    char store[4096] = {0};", (char *)&store);
-    prs("%s", (char *)&store);
+    char s[4096] = "#include <stdio.h>\n#include <string.h>\nvoid puts_m(const char *s) {\n    int i;\n    for(i=0; i<strlen(s); i++) {\n        if (s[i] == 10) {\n            putchar(92);\n            putchar(110);\n        } else {\n            putchar(s[i]);\n        }\n    }\n}\nvoid prs(char *s) {\n    const char tail[] = {34, 59, 10, 32, 32, 32, 32, 112, 114, 115, 40, 115, 41, 59, 10, 125};\n    fputs(s, stdout);\n    putchar(34);\n    puts_m(s);\n    puts(tail);\n}\nvoid main() {\n    char s[4096] = ";
+    prs(s);
 }
